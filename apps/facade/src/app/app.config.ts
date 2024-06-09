60b7eaab7d1fbb3bb29c0f22f631859c788d7a2a
Sultanefendi <sedredinovich@gmail.com>
2024-06-09 18:07:38 +0300

    Подключил NGRX

--- apps/facade/src/app/app.config.ts
+++ apps/facade/src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TuiRootModule } from '@taiga-ui/core';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -13,6 +18,18 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(),
+    provideStore({
+      router: routerReducer
+    }),
+    provideRouterStore(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
     provideAnimations(),
     provideRouter(appRoutes),
     importProvidersFrom(
